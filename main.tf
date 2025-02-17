7596557049280ef7fc6dcb1545d6406fcab3c3c8
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-02-17 10:12:49 +0500

    adding access to azure credential through SP

--- main.tf
+++ main.tf
@@ -122,7 +122,53 @@ resource "azurerm_virtual_machine" "ta-vm" {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
-    version   = "latest"
+    version   = "latest"terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.18.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "ta-resources"
+    storage_account_name = "tastorageaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "ta-rg" {
+  name     = "ta-resources"
+  location = "West Europe"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_account" "ta_storage" {
+  name                     = "tastorageaccount"
+  resource_group_name      = azurerm_resource_group.ta-rg.name
+  location                 = azurerm_resource_group.ta-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.ta_storage.name
+  container_access_type = "private"
+}
   }
 
   storage_os_disk {
